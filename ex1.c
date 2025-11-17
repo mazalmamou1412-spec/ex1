de6386605885c3a69794c9cc6f02466674f7fabe
mazalmamou1412-spec <mazal.mamou1412@gmail.com>
2025-11-17 17:44:32 +0200

    Update ex1.c

--- ex1.c
+++ ex1.c
@@ -14,53 +14,84 @@ int main()
 // define the data type and the variable
     char character;  
   printf("\nAscii:\n");
-    // Instructions to the user
+       // asking the user to enter a character that will later be the value of the variable "character"
 printf("Please enter a character: \n"); 
-    // asking the user to enter a character that will later be the value of the variable "character"
     scanf ("%c", &character);
+    
     //giving the numerical value from ascii table of the input
     printf("It's numerical value is : %d\n", character); 
+    
     // defining if the numerical value of the input is even or odd
     printf("0 for even, 1 for odd: %d\n",(character&1) ); 
 
+
+
+    
    // Task 2 :  2's complement and other representations
     
 // defining variable
    int number=0; 
     printf("\n\n2's complement to other representations:\n"); 
-   // instruction
+
+    // asking the user to enter an integer that will be the value of the variable "number"    
     printf("Please enter a negative integer : \n");
-// asking the user to enter an integer that will be the value of the variable "number"
     scanf("%d", &number); 
+
     // calculating the 1's complement value of the input
     printf("1's complement : %d\n", -(~number));
     // calculating the unsigned value of the input
     printf("Unsigned : %u\n", number);
 
+
+
+
+    
 // Task 3 : Shifting right and left
 
  // defining variables   
 int first, second, third, result;
     printf("\nShifting right and left\n");
-    // instructions 
+    // asking the user to enter 3 integers who's gonna be the values of the variables : first second and third    
     printf("please enter 3 integers : \n"); 
-    scanf("%d%d%d", &first, &second, &third); // asking the user to enter 3 integers who's gonna be the values of the variables : first second and third
-    result=first>>second<<third; // calculating the shiftings
-    printf("After shifting right and left : %d\n", result); // final result
+    scanf("%d%d%d", &first, &second, &third); 
+    
+    // calculating the shiftings and printing the final result    
+    result=first>>second<<third; 
+    printf("After shifting right and left : %d\n", result); 
 
+    
+    
+    
+    
     // Task 4 : even and odd
+
+    //defining variables
     int one, two, three;
     printf("\nEven and Odd");
-    printf("\nplease enter 3 integers : "); // instructions to the user
-    scanf("%d%d%d", &one, &two, &three);
-    // asking the user to enter 3 integers who's gonna be the values of the variables : one, two and three
-    printf("0 - most of them are even, 1 - most of them are odd: %d\n",(one&1)&(two&1)|(one&1)&(three&1)|(two&1)&(three&1) ); // defining if the most is even or odd
 
+     /instructions to the user to collect the values of the variables : "one", "two" and "three"
+    printf("\nplease enter 3 integers : "); 
+    scanf("%d%d%d", &one, &two, &three);
+
+    // defining if the most is even or odd
+    printf("0 - most of them are even, 1 - most of them are odd: %d\n",(one&1)&(two&1)|(one&1)&(three&1)|(two&1)&(three&1) ); 
+
+ 
+    
+    
+    
+    
     // Task 5 : Different Bases
+
+    // defining variables
     int octalNumber, hexNumber, ocNumMSB, hexNumMSB;
     printf("\nDifferent bases\n");
+
+    // instructions to the user to collect the values of : "octalNumer" and "hexNumber"
     printf("Please enter 2 numbers in octal and hexadecimal bases : ");
     scanf("%o%x", &octalNumber, &hexNumber);
+
+    //calculating and printing the LSBs and MSBs of the previous inputs
     printf("\nLSBs : %d, %d",octalNumber&1, hexNumber&1);
     ocNumMSB=octalNumber>>31;
     hexNumMSB=hexNumber>>31;
